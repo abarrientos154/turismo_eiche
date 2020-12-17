5ef13180199477358e02b5e9859d99a8f3630085
example <example@example.com>
2020-12-16 21:42:59 -0400

    16/12/2020

---
+++ client/src/components/DetalleCategoria.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+<div class="q-pa-md row items-start q-gutter-md">
+    <q-card class="my-card">
+      <img src="https://cdn.quasar.dev/img/mountains.jpg">
+
+      <q-card-section>
+        <div class="text-h6">Our Changing Planet</div>
+        <div class="text-subtitle2">by John Doe</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        {{ lorem }}
+      </q-card-section>
+    </q-card>
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['titulo', 'Descripcion', 'imagen'],
+  data () {
+    return {
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>>
