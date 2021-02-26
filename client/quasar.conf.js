ce4f2d95699e0fac58fd4b6afee8f8f5af21c79d
example <example@example.com>
2021-02-26 12:21:59 -0400

    titulo

--- client/quasar.conf.js
+++ client/quasar.conf.js
@@ -117,8 +117,8 @@ module.exports = function (/* ctx */) {
       workboxPluginMode: 'GenerateSW', // 'GenerateSW' or 'InjectManifest'
       workboxOptions: {}, // only for GenerateSW
       manifest: {
-        name: 'Quasar App',
-        short_name: 'Quasar App',
+        name: 'Turismo Mejillones',
+        short_name: 'Turismo Mejillones',
         description: 'A Quasar Framework app',
         display: 'standalone',
         orientation: 'portrait',
